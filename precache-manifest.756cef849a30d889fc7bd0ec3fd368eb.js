cd44a544fc23dba76562bd164fcba6da5aced9e4
Anshu S Panda <anshuspanda@gmail.com>
2019-10-05 22:28:19 +0530

    Updates

---
+++ precache-manifest.756cef849a30d889fc7bd0ec3fd368eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f298f9c4062264176b66d1b6089e7ce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c3db4b6f555377945d3",
+    "url": "/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "770fba54f00eb5469e49",
+    "url": "/static/css/main.d70a1adf.chunk.css"
+  },
+  {
+    "revision": "6c3db4b6f555377945d3",
+    "url": "/static/js/2.d3320c63.chunk.js"
+  },
+  {
+    "revision": "770fba54f00eb5469e49",
+    "url": "/static/js/main.acc6932b.chunk.js"
+  },
+  {
+    "revision": "b99d4eac875820cfca5a",
+    "url": "/static/js/runtime~main.f3c983a4.js"
+  },
+  {
+    "revision": "e1823ff088795dab942c250c16f47f18",
+    "url": "/static/media/card-icon.e1823ff0.png"
+  },
+  {
+    "revision": "198ac92017765eeb663abad26a46de9c",
+    "url": "/static/media/whitepanda-logo.198ac920.png"
+  }
+]);
